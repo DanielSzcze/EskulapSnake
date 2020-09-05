a435759bd8da7ac58c69ad9e60d769376bcea05f
Piotrek <p.palian@o2.pl>
2020-09-05 10:15:20 +0200

    changed repository

--- src/main/java/pl/EskulapSnake/repository/UserRepository.java
+++ src/main/java/pl/EskulapSnake/repository/UserRepository.java
@@ -3,5 +3,5 @@ package pl.EskulapSnake.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.EskulapSnake.model.User;
 
-public interface UserRepository implements JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
