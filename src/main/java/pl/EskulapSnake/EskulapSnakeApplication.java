91fff11e4755bdbdcd7197fd9812f5488e859cae
DanielSz <daniel.szczesny1@gmail.com>
2020-09-05 08:59:54 +0200

    Inicjalizacja

---
+++ src/main/java/pl/EskulapSnake/EskulapSnakeApplication.java
@@ -0,0 +1,13 @@
+package pl.EskulapSnake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskulapSnakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskulapSnakeApplication.class, args);
+	}
+
+}
