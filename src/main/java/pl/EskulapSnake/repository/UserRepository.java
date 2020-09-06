e0f606cf6ad655a4046f9963ea839aafed99d47c
rafal456 <53346143+rafal456@users.noreply.github.com>
2020-09-06 12:39:08 +0200

    Update UserRepository.java

--- src/main/java/pl/EskulapSnake/repository/UserRepository.java
+++ src/main/java/pl/EskulapSnake/repository/UserRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository   <User, Long> {
 
-    @Query()
     Optional<User>findUserByUserName(String userName);
 }
