ab35d597270243da561469173669148e2bf2124d
Piotrek <p.palian@o2.pl>
2020-09-06 11:21:25 +0200

    changed patient elements

--- src/main/java/pl/EskulapSnake/service/PatientService.java
+++ src/main/java/pl/EskulapSnake/service/PatientService.java
@@ -1,12 +1,62 @@
 package pl.EskulapSnake.service;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.EskulapSnake.dto.PatientDto;
+import pl.EskulapSnake.model.Patient;
 import pl.EskulapSnake.repository.PatientRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class PatientService {
-    private  PatientRepository patientRepository;
+    private PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> findAll() {
+        return patientRepository.findAll();
+    }
+
+    public Patient findById(Long id) {
+        return patientRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("In db is no Patient with this id"));
+    }
+
+    @Transactional
+    public void deleteAll() {
+        patientRepository.deleteAll();
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        patientRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Patient createNew(PatientDto patientDto) {
+        Patient patientToSave = getEntityFromDto(patientDto);
+        patientRepository.save(patientToSave);
+        return patientToSave;
+    }
+
+    @Transactional
+    public Patient update(Long id, PatientDto patientDto) {
+        Patient patientToUpdate = getEntityFromDto(patientDto);
+        patientToUpdate.setId(id);
+        patientRepository.save(patientToUpdate);
+        return patientToUpdate;
+    }
+
+
+    private Patient getEntityFromDto(PatientDto patientDto) {
+        Patient result = new Patient();
+        result.setFirstName(patientDto.getFirstName());
+        result.setLastName(patientDto.getLastName());
+        result.setPesel(patientDto.getPesel());
+        return result;
     }
 }
