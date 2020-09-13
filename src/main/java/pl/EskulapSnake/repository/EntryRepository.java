ce14c5e3f3c66e646f13fa9d12894fce4a0b00bf
rafal456 <53346143+rafal456@users.noreply.github.com>
2020-09-13 14:20:20 +0200

    Update EntryRepository.java

--- src/main/java/pl/EskulapSnake/repository/EntryRepository.java
+++ src/main/java/pl/EskulapSnake/repository/EntryRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface EntryRepository  extends JpaRepository<Entry, Long> {
 
 
-    @Query("SELECT en FROM  Entry en JOIN  en.employeeId em WHERE  em.id = :employee_id AND en.localDateTime BETWEEN :from_date and  :to_date")
+    @Query("SELECT en FROM  Entry en JOIN  en.employee em WHERE  em.id = :employee_id AND en.localDateTime BETWEEN :from_date and  :to_date")
     public List<Entry> getByEmpIdAndTimeInterval(@Param("employee_id") Long EmployeeId,
                                                    @Param("from_date") LocalDateTime BeginMonth,
                                                    @Param("to_date") LocalDateTime EndMonth);
